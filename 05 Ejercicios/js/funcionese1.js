ae11e95bd637eb7c8bfa350f7e9117e3f5c8d4d0
GarciaPiedraEdwinLeonardo <egarciap2306@alumno.ipn.mx>
2025-10-06 20:36:44 -0600

    Ejercicios

---
+++ 05 Ejercicios/js/funcionese1.js
@@ -0,0 +1,25 @@
+function validarn(e){
+    var teclado = document.all ? e.keyCode : e.which;
+    if(teclado == 8) return true;
+    var patron = /[0-9\d .]/; //Falta validar que no pueda poner mas de un punto
+
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function interes(){
+    var valor = document.getElementById("cantidadi").value;
+    alert(valor);
+    var parseo = parseFloat(valor);
+    alert(parseo);
+    var interes = parseo*(0.085);//LIMITE A DOS DECIMALES
+    alert(interes);
+    var total = interes + parseo;
+    alert(total);
+    document.getElementById("saldoi").value = "$ " + total; //LIMITE A DOS DECIMALES
+}
+
+function borrari(){
+    document.getElementById("saldoi").value = "";
+    document.getElementById("cantidadi").value = "";
+}
